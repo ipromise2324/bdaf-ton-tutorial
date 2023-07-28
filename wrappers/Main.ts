a97982477f427c713494500b3deb2fe330921e8d
ipromise2324 <168allen55@gmail.com>
2023-07-29 01:45:15 +0800

    fix: didn't write config variable

--- wrappers/Main.ts
+++ wrappers/Main.ts
@@ -1,9 +1,17 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
-
-export type MainConfig = {};
+import { Opcodes } from '../helpers/Opcodes';
+export type MainConfig = {
+    seqno: number;
+    publicKey: Buffer;
+    ownerAddress: Address;
+};
 
 export function mainConfigToCell(config: MainConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeUint(config.seqno,32)
+        .storeBuffer(config.publicKey)
+        .storeAddress(config.ownerAddress)
+    .endCell();
 }
 
 export class Main implements Contract {
@@ -26,4 +34,97 @@ export class Main implements Contract {
             body: beginCell().endCell(),
         });
     }
+    async sendDeposit(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.deposit, 32)
+            .endCell(),
+        });
+    }
+
+    async sendWithdraw (provider: ContractProvider, via: Sender, 
+        opts:{
+            value: bigint,
+            amount: bigint
+        }) 
+    {
+        await provider.internal(via, {
+            value:opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.withdrawFunds, 32)
+                .storeCoins(opts.amount)
+            .endCell(),
+        });
+    }
+
+    async sendChangeOwner (provider: ContractProvider, via: Sender,
+        opts:{
+            value: bigint,
+            newOwner: Address
+        })
+    {
+        await provider.internal(via, {
+            value:opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.changeOwner, 32)
+                .storeAddress(opts.newOwner)
+            .endCell(),
+        });
+    }
+
+    async sendMessageToOwner (provider: ContractProvider, via: Sender,
+        opts:{
+            value: bigint,
+        })
+    {
+        await provider.internal(via, {
+            value:opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.transferMsgToOwner, 32)
+            .endCell(),
+        });
+    }
+
+    async sendExtMessage (provider: ContractProvider,
+        opts:{
+            opCode: number,
+            seqno: number,
+            signFunC: (buf: Buffer) => Buffer,
+        })
+    {
+        const msgToSign = beginCell()
+                .storeUint(opts.seqno, 32)
+                .storeUint(opts.opCode, 32)
+            .endCell();
+        const sig = opts.signFunC(msgToSign.hash());
+
+        await provider.external(
+            beginCell()
+                .storeBuffer(sig)
+                .storeSlice(msgToSign.asSlice())
+            .endCell()
+        );
+    }
+
+    async getBalance(provider: ContractProvider): Promise<bigint> {
+        const balance = await provider.get('get_smc_balance', []);
+        return balance.stack.readBigNumber();
+    }
+
+    async getSeqno(provider: ContractProvider): Promise<number> {
+        const seqno = await provider.get('get_seqno', []);
+        return seqno.stack.readNumber();
+    }
+
+    async getOwner(provider: ContractProvider): Promise<Address> {
+        const owner = await provider.get('get_owner', []);
+        return owner.stack.readAddress();
+    }
 }
+
+
