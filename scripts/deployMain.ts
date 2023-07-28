52bb445aee70538da03cb2607da18cf5706d48d8
ipromise2324 <168allen55@gmail.com>
2023-07-28 22:28:31 +0800

    first commit

---
+++ scripts/deployMain.ts
@@ -0,0 +1,13 @@
+import { toNano } from 'ton-core';
+import { Main } from '../wrappers/Main';
+import { compile, NetworkProvider } from '@ton-community/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const main = provider.open(Main.createFromConfig({}, await compile('Main')));
+
+    await main.sendDeploy(provider.sender(), toNano('0.05'));
+
+    await provider.waitForDeploy(main.address);
+
+    // run methods on `main`
+}
