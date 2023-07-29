b5744036e9bc114c499e3ef7955d4e0a6154c2a6
ipromise2324 <168allen55@gmail.com>
2023-07-29 13:06:13 +0800

    feat: deploy smart contract on TON Testnet

--- scripts/deployMain.ts
+++ scripts/deployMain.ts
@@ -1,9 +1,14 @@
-import { toNano } from 'ton-core';
+import { Address,toNano } from 'ton-core';
 import { Main } from '../wrappers/Main';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { createKeys } from '../helpers/key';
 
 export async function run(provider: NetworkProvider) {
-    const main = provider.open(Main.createFromConfig({}, await compile('Main')));
+    const main = provider.open(Main.createFromConfig({
+        seqno: 0,
+        publicKey: (await createKeys()).publicKey,
+        ownerAddress: Address.parse('kQAHg-2Oy8Mc2BfENEaBcoDNXvHCu7mc28KkPIks8ZVqwjEl')
+    }, await compile('Main')));
 
     await main.sendDeploy(provider.sender(), toNano('0.05'));
 
